9f644b3113db2ce245b8783b5b8f157e0b3f9ac5
Max <lot.for.life@gmail.com>
2014-08-05 22:36:59 +0400

    way2

--- way/script.js
+++ way/script.js
@@ -1,4 +1,41 @@
 console.log("run");
 
-var cellSize = 32;
-var $cellTemplate = $("<span />").addClass("cell").width(cellSize).height(cellSize);
+//var map = document.getElementById("map"),
+//    ctx = map.getContext('2d');
+//    map.width = 100;
+//    map.height = 100;
+//
+//
+//var cellSize = 32;
+////var $cellTemplate = $("<span />").addClass("cell").width(cellSize).height(cellSize);
+//var temp = 10;
+//
+//for (var r=0; r<temp; r++){
+//
+//    for (var c=0; c<temp; c++){
+//
+//    }
+//}
+//
+//ctx.fillRect(0, 0, 10, 10);
+
+var canvas = document.getElementById("canvas"),
+    ctx = canvas.getContext('2d');
+canvas.width = 400;
+canvas.height = 400;
+
+x = {x1:0,
+    x2:400};
+
+y = {y1:0,
+    y2:400};
+
+ctx.beginPath();
+ctx.moveTo(x.x1, y.y1);
+ctx.lineTo(x.x2, y.y2);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(x.x2, y.y1);
+ctx.lineTo(x.x1, y.y2);
+ctx.stroke();
